22465564ec24b6016b67ff35b359e4dc905e4034
kozamonq <mathiaslac@gmail.com>
2022-08-29 03:53:21 +0200

    count

--- routes/routers/ServersRouter.js
+++ routes/routers/ServersRouter.js
@@ -3,8 +3,17 @@ const ServersController = require("../../controllers/ServersController");
 const issetServerMiddleware = require("../../middleware/issetServerMiddleware");
 const router = new Router();
 
-router.get("/:server/ranks", issetServerMiddleware, (...args) => ServersController.ranks(...args));
-router.get("/:server/info", issetServerMiddleware, (...args) => ServersController.monitoring(...args));
-router.get("/:server/players", issetServerMiddleware, (...args) => ServersController.monitoring_players(...args));
+router.get("/:server/ranks", issetServerMiddleware, (...args) =>
+  ServersController.ranks(...args)
+);
+router.get("/:server/count", issetServerMiddleware, (...args) =>
+  ServersController.count(...args)
+);
+router.get("/:server/info", issetServerMiddleware, (...args) =>
+  ServersController.monitoring(...args)
+);
+router.get("/:server/players", issetServerMiddleware, (...args) =>
+  ServersController.monitoring_players(...args)
+);
 
 module.exports = router;
