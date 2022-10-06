53005b678cca7ea3f2b24a9426c7d0cb8b214113
kozamonq <mathiaslac@gmail.com>
2022-10-07 01:23:30 +0200

    Update ShopController.js

--- controllers/ShopController.js
+++ controllers/ShopController.js
@@ -3,8 +3,7 @@ const { All } = require("../config/db");
 class ShopController {
   async get(req, res, next) {
     All.query(
-      "SELECT * FROM shop_players WHERE auth LIKE ?",
-      `${req.params["steamID"]}`,
+      `SELECT * FROM shop_players WHERE auth LIKE "%${req.params["steamID"]}%"`,
       (err, results) => {
         if (err) {
           console.log(err);
