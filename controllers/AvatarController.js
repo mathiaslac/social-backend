73021a205c38c5f683e25ddf0568d28bf9ffc64a
Nota <harenkeshaav11@gmail.com>
2022-10-10 02:35:40 +0600

    return all avatars

--- controllers/AvatarController.js
+++ controllers/AvatarController.js
@@ -2,9 +2,19 @@ const axios = require("axios");
 class AvatarController {
   async get(req, res, next) {
     const data = await axios.get(
-      `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.STEAM_API_KEY}&steamids=${req.query["steamID"]}`
+      `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.STEAM_API_KEY}&steamids=${req.query["steamIDS"]}`
     );
-    return res.json(data.data.response.players[0].avatarfull);
+    
+    const filter = data.data.response.players.map((user) => {
+      return {
+        steamid: user['steamid'],
+        avatarfull: user['avatarfull']
+      }
+    })
+    
+    return res.json({
+      players: filter
+    })
   }
 }
 
